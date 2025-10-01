9c278239b2bc045dd385dcfc0646d0010f8bc128
beem35 <beemmlg1234@gmail.com>
2025-10-01 07:20:59 +0700

    code-hw

---
+++ LAB/Lab-1/1-1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int A ;
+    int B ;
+    int C ;
+    printf( "Enter number [1]: " );
+    scanf( "%d", &A ) ;
+    printf( "Enter number [2]: " ) ;
+    scanf( "%d", &B ) ;
+    printf( "Enter number [3]: " ) ;
+    scanf( "%d", &C ) ;
+    printf( "Answer = %d \n", A+B+C ) ; 
+    return 0 ;
+}//end main function
