6d50d4c6e569eee72d6701e00b569f505e3989f9
beem35 <beemmlg1234@gmail.com>
2025-10-03 16:57:04 +0700

    goodtime

--- LAB/Lab-15/15.cpp
+++ LAB/Lab-15/15.cpp
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <ctype.h> // สำหรับ isspace
 
-int countWordsInFile(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("ไม่สามารถเปิดไฟล์ %s ได้\n", filename);
-        return -1;
-    }
-
-    int wordCount = 0;
-    int inWord = 0; // flag ว่ากำลังอยู่ในคำหรือไม่
-    char ch;
-
-    while ((ch = fgetc(file)) != EOF) {
-        if (isspace(ch)) { // ถ้าเจอ whitespace
-            inWord = 0;    // สิ้นสุดคำ
-        } else {
-            if (!inWord) {
-                wordCount++; // เจอเริ่มคำใหม่
-                inWord = 1;
-            }
-        }
-    }
-
-    fclose(file);
-    return wordCount;
-}
+int countword( char * ) ;
 
 int main() {
     char name[10] ;
 
     printf("Enter file name: ") ;
     scanf( "%s" , name ) ;
-    const char *filename = name;
     
-    int totalWords = countWordsInFile(filename);
-    if (totalWords >= 0) {
-        printf("Total number of words in '%s' : %d words\n", filename, totalWords);
+    int totalword = countword(name);
+    if (totalword >= 0) {
+        printf("Total number of words in '%s' : %d words\n", name, totalword) ;
     }
     return 0;
+}
+
+int countword( char *filename ) {
+    FILE *file = fopen(filename, "r") ;
+    if ( !file ) {
+        printf("ไม่สามารถเปิดไฟล์ %s ได้\n", filename) ;
+        return -1 ;
+    }
+
+    int word_count = 0 ;
+    int inword = 0 ; // flag ว่ากำลังอยู่ในคำหรือไม่
+    char ch ;
+
+    while ( ( ch = fgetc(file) ) != EOF ) {
+        if ( isspace(ch) ) { // ถ้าเจอ whitespace
+            inword = 0;    // สิ้นสุดคำ
+        } else {
+            if ( !inword ) {
+                word_count++ ; // เจอเริ่มคำใหม่
+                inword = 1 ;
+            }
+        }
+    }
+
+    fclose(file) ;
+
+    return word_count ;
 }
